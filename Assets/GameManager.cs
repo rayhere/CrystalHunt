add4bd5fecd3e7723e37efa7c35812bee9f7fc07
Hong Lam To <raymond0586@msn.com>
2024-06-10 22:11:46 -0400

    GameManager.cs: Complete fix for initializing ObjectPool for crystal, cabbage, and stonecube prefabs.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public CabbageController cabbagePrefab;
-    public AutoRollCubeToTarget stoneCubePrefab;
+    public CrystalController crystalPrefab;
+    public StoneCubeController stoneCubePrefab;
+    public GameObject targetObject;
 
     private void Awake()
     {
         // SetupPool();
         SetupGreenCabbagePool(6);
-        SetupStoneCubePool(stoneCubePrefab, 5, "StoneCube");
+        //SetupStoneCubePool(stoneCubePrefab, 5, "StoneCube");
+
+        // Find the target object using tag
+        targetObject = GameObject.FindGameObjectWithTag("StoneCube");
+
+        // Setup Stone Cube pool with the found target object
+        SetupStoneCubePool(stoneCubePrefab, 5, "StoneCube", targetObject.transform);
+        //SetupStoneCubePool(stoneCubePrefab, 5, "StoneCube", target);
+
+        SetupCrystalPool(crystalPrefab, 5, "Crystal");
     }
 
     private void SetupPool()
@@ -44,30 +55,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void SetupStoneCubePool<T>(T pooledItemPrefab, int poolSize, string poolDictionary) where T : Component
-    {
-        ObjectPooler.SetupPool(pooledItemPrefab, poolDictionary);
-
-        for (int i = 0; i < poolSize; i++)
-        {
-            T newInstance = ObjectPooler.EnqueueNewInstance<T>(pooledItemPrefab, poolDictionary);
-
-            if (newInstance != null)
-            {
-                newInstance.transform.SetParent(transform, false);
-                
-                // Check if the instance is of type AutoRollCubeToTarget
-                if (newInstance is AutoRollCubeToTarget)
-                {
-                    AutoRollCubeToTarget cubeInstance = newInstance as AutoRollCubeToTarget;
-                    cubeInstance.DeactiveEmptyObj();
-                }
-
-                newInstance.gameObject.SetActive(false); // Accessing the GameObject directly to set deactive
-            }
-        }
-    }
-
     private void SetupGreenCabbagePool(int poolSize)
     {
         ObjectPooler.SetupPool(cabbagePrefab, "Cabbage");
@@ -83,4 +70,54 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void SetupCrystalPool<T>(T pooledItemPrefab, int poolSize, string poolDictionary) where T : Component
+    {
+        ObjectPooler.SetupPool(pooledItemPrefab, poolDictionary);
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            T newInstance = ObjectPooler.EnqueueNewInstance<T>(pooledItemPrefab, poolDictionary);
+
+            if (newInstance != null)
+            {
+                newInstance.transform.SetParent(transform, false);
+                
+                // Check if the instance is of type T
+                if (newInstance is T)
+                {
+                    T objInstance = newInstance as T;
+                    // CALL Prefab Method
+                    //objInstance.method();
+                }
+
+                newInstance.gameObject.SetActive(false); // Accessing the GameObject directly to set deactive
+            }
+        }
+    }
+
+    private void SetupStoneCubePool<T>(T pooledItemPrefab, int poolSize, string poolDictionary, Transform target) where T : Component
+    {
+        ObjectPooler.SetupPool(pooledItemPrefab, poolDictionary);
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            T newInstance = ObjectPooler.EnqueueNewInstance<T>(pooledItemPrefab, poolDictionary);
+
+            if (newInstance != null)
+            {
+                newInstance.transform.SetParent(transform, false);
+                
+                // Check if the instance is of type StoneCubeController
+                if (newInstance is StoneCubeController)
+                {
+                    StoneCubeController cubeInstance = newInstance as StoneCubeController;
+                    cubeInstance.SetTarget(target); // Set the target for the stone cube
+                    cubeInstance.SetEmptyObjectToParent();
+                    cubeInstance.DeactiveEmptyObj();
+                }
+
+                newInstance.gameObject.SetActive(false); // Set the GameObject inactive
+            }
+        }
+    }
 }
---
+++ Assets/StoneCubeController.cs
@@ -0,0 +1,247 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class StoneCubeController : MonoBehaviour
+{
+    private Vector3 offset;
+
+    public Transform[] Targets; // Target positions for cube movement
+
+    public GameObject _cube;
+    private GameObject center;
+    private GameObject up;
+    private GameObject down;
+    private GameObject left;
+    private GameObject right;
+
+    private GameObject emptyGameObject; // Allow to control this object even _cube return to pool
+
+    public float lengthOfCube = 1f;
+
+    public int step = 9;
+    public float speed = 0.01f;
+
+    private bool input = false;
+
+    private void Awake()
+    {
+        CreateEmptyObject();
+    }
+
+    void Start()
+    {
+        
+        StartCoroutine(StartMovementTimer());
+    }
+
+    void Update()
+    {
+        if (input)
+        {
+            StartCoroutine(ChooseAndMove());
+        }
+    }
+
+    IEnumerator ChooseAndMove()
+    {
+        // Disable input while choosing and moving
+        input = false;
+
+        // Choose a random target
+        int randomIndex = UnityEngine.Random.Range(0, Targets.Length);
+        Vector3 targetPosition = Targets[randomIndex].position;
+
+        // Calculate the direction to the target
+        Vector3 direction = (targetPosition - _cube.transform.position).normalized;
+
+        // Check if the target position is at Vector3.zero
+        if (targetPosition == Vector3.zero)
+        {
+            // Choose a random direction if the target is at Vector3.zero
+            direction = UnityEngine.Random.onUnitSphere;
+        }
+
+        // Determine which direction to move based on the dot product with the world axes
+        float dotUp = direction.x;
+        float dotDown = Vector3.Dot(direction, Vector3.down);
+        float dotLeft = Vector3.Dot(direction, Vector3.left);
+        float dotRight = Vector3.Dot(direction, Vector3.right);
+
+        /* Debug.Log("direction is " + direction + " Vector3 up is " + Vector3.up);
+        Debug.Log("dotUp is " + dotUp + " Vector3 up is " + Vector3.up);
+        Debug.Log("dotDown is " + dotDown + " Vector3 down is " + Vector3.down);
+        Debug.Log("dotLeft is " + dotLeft + " Vector3 left is " + Vector3.left);
+        Debug.Log("dotRight is " + dotRight + " Vector3 right is " + Vector3.right); */
+
+        //Debug.Log("Mathf.Abs(direction.x) " + Mathf.Abs(direction.x));
+        //Debug.Log("Mathf.Abs(direction.z) " + Mathf.Abs(direction.z));
+        if (Mathf.Abs(direction.x) ==0 && Mathf.Abs(direction.z) ==0 )
+        {
+            // don't do anything
+            //Debug.Log("don't do anything " + direction);
+        }
+        if (Mathf.Abs(direction.x) >= Mathf.Abs(direction.z))
+        {
+            // move Left or Right
+            //Debug.Log("move Left or Right " + direction);
+            if (direction.x > 0)
+            {
+                yield return StartCoroutine(moveRight());
+            }
+            else if (direction.x < 0)
+            {
+                yield return StartCoroutine(moveLeft());
+            }
+        }
+        else if (Mathf.Abs(direction.x) <= Mathf.Abs(direction.z))
+        {
+            // move Up or Down
+            //Debug.Log("move Up or Down " + direction);
+            if (direction.z > 0)
+            {
+                yield return StartCoroutine(moveUP());
+            }
+            else if (direction.z < 0)
+            {
+                yield return StartCoroutine(moveDown());
+            }
+        }
+
+        // Wait for 5 seconds before choosing the next target
+        yield return new WaitForSeconds(3f);
+
+
+        // Enable input after moving
+        input = true;
+    }
+
+    IEnumerator moveUP()
+    {
+        for (int i = 0; i < (90 / step); i++)
+        {
+            _cube.transform.RotateAround(up.transform.position, Vector3.right, step);
+            yield return new WaitForSeconds(speed);
+        }
+        center.transform.position = _cube.transform.position;
+    }
+
+    IEnumerator moveDown()
+    {
+        for (int i = 0; i < (90 / step); i++)
+        {
+            _cube.transform.RotateAround(down.transform.position, Vector3.left, step);
+            yield return new WaitForSeconds(speed);
+        }
+        center.transform.position = _cube.transform.position;
+    }
+
+    IEnumerator moveLeft()
+    {
+        for (int i = 0; i < (90 / step); i++)
+        {
+            _cube.transform.RotateAround(left.transform.position, Vector3.forward, step);
+            yield return new WaitForSeconds(speed);
+        }
+        center.transform.position = _cube.transform.position;
+    }
+
+    IEnumerator moveRight()
+    {
+        for (int i = 0; i < (90 / step); i++)
+        {
+            // Spin the object around the right position in number of step
+            _cube.transform.RotateAround(right.transform.position, Vector3.back, step);
+            yield return new WaitForSeconds(speed);
+        }
+        center.transform.position = _cube.transform.position;
+    }
+
+    public void CreateEmptyObject()
+    {
+        lengthOfCube = Mathf.Abs(lengthOfCube / 2);
+        // Create an empty GameObject
+        //GameObject emptyGameObject = new GameObject(gameObject.name + " Center");
+        emptyGameObject = new GameObject(gameObject.name + " Center");
+        
+        // Create empty GameObjects for Up, Down, Left, and Right
+        GameObject upObject = new GameObject("Up");
+        GameObject downObject = new GameObject("Down");
+        GameObject leftObject = new GameObject("Left");
+        GameObject rightObject = new GameObject("Right");
+
+        // assign empty Gameobjet
+        center = emptyGameObject;
+        up = upObject;
+        down = downObject;
+        left = leftObject;
+        right = rightObject;
+
+        // Set the parent of Up, Down, Left, and Right to EmptyGameObject
+        upObject.transform.parent = emptyGameObject.transform;
+        downObject.transform.parent = emptyGameObject.transform;
+        leftObject.transform.parent = emptyGameObject.transform;
+        rightObject.transform.parent = emptyGameObject.transform;
+        
+        // Set the position, rotation, and scale of EmptyGameObject to match _cube
+        emptyGameObject.transform.position = _cube.transform.position;
+        emptyGameObject.transform.rotation = _cube.transform.rotation;
+        emptyGameObject.transform.localScale = _cube.transform.localScale;
+
+        // Set the positions of Up, Down, Left, and Right relative to EmptyGameObject
+        upObject.transform.localPosition = new Vector3(0, -lengthOfCube, lengthOfCube);
+        downObject.transform.localPosition = new Vector3(0, -lengthOfCube, -lengthOfCube);
+        leftObject.transform.localPosition = new Vector3(-lengthOfCube, -lengthOfCube, 0);
+        rightObject.transform.localPosition = new Vector3(lengthOfCube, -lengthOfCube, 0);
+    }
+
+    IEnumerator StartMovementTimer()
+    {
+        // Wait for 5 seconds before starting the movement
+        yield return new WaitForSeconds(2f);
+
+        // Start the movement
+        input = true;
+    }
+
+    public void ActiveEmptyObj()
+    {
+        ResetEmptyObj();
+        emptyGameObject.SetActive(true);
+    }
+
+    public void DeactiveEmptyObj()
+    {
+        emptyGameObject.SetActive(false);
+    }
+
+    private void ResetEmptyObj()
+    {
+        // Set the position, rotation, and scale of EmptyGameObject to match _cube
+        emptyGameObject.transform.position = _cube.transform.position;
+        emptyGameObject.transform.rotation = _cube.transform.rotation;
+        emptyGameObject.transform.localScale = _cube.transform.localScale;
+    }
+
+    public void SetEmptyObjectToParent()
+    {
+        // Check if the parent of StoneCubeController object is not null
+        if (transform.parent != null)
+        {
+            // Set the parent of emptyGameObject to be the same as the parent of StoneCubeController
+            emptyGameObject.transform.SetParent(transform.parent);
+        }
+    }
+
+    public void SetTargets(Transform[] targets)
+    {
+        Targets = targets;
+    }
+
+    public void SetTarget(Transform target)
+    {
+        Targets = new Transform[] { target };
+    }
+}
